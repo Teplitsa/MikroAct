af42253b170be5b3bcd114dec5fc5445e68f8b97
Carl van Tonder <carl@supervacuo.com>
2013-01-23 16:46:25 -0500

    Add basic collection list view

--- acts/urls_collections.py
+++ acts/urls_collections.py
@@ -1,8 +1,9 @@
 # vim: fileencoding=utf-8 ai ts=4 sts=4 et sw=4
 from django.conf.urls import patterns, include, url
 
-from .views import CollectionDetailView
+from .views import CollectionListView, CollectionDetailView
 
 urlpatterns = patterns('',
+    url(r'^$', CollectionListView.as_view(), name='collection_list'),
     url(r'(?P<slug>[-_\w]+)$', CollectionDetailView.as_view(), name='collection_detail'),
 )
--- acts/views.py
+++ acts/views.py
@@ -1,7 +1,14 @@
 # vim: fileencoding=utf-8 ai ts=4 sts=4 et sw=4
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Collection
+
+
+class CollectionListView(ListView):
+   model = Collection
+   context_object_name = 'collections'
+
 
 class CollectionDetailView(DetailView):
     model = Collection
