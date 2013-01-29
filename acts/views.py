77dbf474151e8e8ed2b3d51ea7d97f39afdaac93
Carl van Tonder <carl@supervacuo.com>
2013-01-28 21:23:51 -0500

    Use django_braces to make some views LoginRequired

--- acts/views.py
+++ acts/views.py
@@ -3,6 +3,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
+from braces.views import LoginRequiredMixin
+
 from .models import Collection, MikroAct
 from .forms import MikroActForm
 
@@ -11,11 +13,11 @@ class CollectionListView(ListView):
     model = Collection
 
 
-class CollectionCreateView(CreateView):
+class CollectionCreateView(LoginRequiredMixin, CreateView):
     model = Collection
 
 
-class CollectionUpdateView(UpdateView):
+class CollectionUpdateView(LoginRequiredMixin, UpdateView):
     model = Collection
 
 
@@ -27,16 +29,18 @@ class MikroActListView(ListView):
     model = MikroAct
 
 
-class MikroActCreateView(CreateView):
+class MikroActCreateView(LoginRequiredMixin, CreateView):
     model = MikroAct
     form_class = MikroActForm
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super (MikroActCreateView, self).form_valid(form)
+        return super(MikroActCreateView, self).form_valid(form)
 
-class MikroActUpdateView(UpdateView):
+
+class MikroActUpdateView(LoginRequiredMixin, UpdateView):
     model = MikroAct
+    form_class = MikroActForm
 
 
 class MikroActDetailView(DetailView):
