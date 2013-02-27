5e4a454b5580f4fde4a0af228a024b89d54f1c65
Carl van Tonder <carl@supervacuo.com>
2013-02-27 18:41:52 -0500

    Enable bootstrap datepicker for MikroAct creation

--- acts/forms.py
+++ acts/forms.py
@@ -1,6 +1,8 @@
 # vim: fileencoding=utf-8 ai ts=4 sts=4 et sw=4
 from django import forms
 
+from bootstrap_toolkit.widgets import BootstrapDateInput
+
 from .models import MikroAct, List
 
 
@@ -10,7 +12,8 @@ class MikroActForm(forms.ModelForm):
         fields = ("title", "slug", "date", "description", "location_address",
                   "status", "is_published", "photo")
         widgets = {
-            "slug": forms.TextInput(attrs={"data-slug-from": "title"})
+            "slug": forms.TextInput(attrs={"data-slug-from": "title"}),
+            "date": BootstrapDateInput()
         }
 
 
