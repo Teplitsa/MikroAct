28f2be4495ca0c6cef511470b6b5517fd2af814b
Carl van Tonder <carl@supervacuo.com>
2013-03-11 17:23:13 -0400

    Automatic deployment with Fabric

--- mikroact/wsgi.py
+++ mikroact/wsgi.py
@@ -14,19 +14,32 @@ framework.
 
 """
 import os
+import sys
+import site
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "mikroact.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mikroact.settings")
+# remember original sys.path.
+prev_sys_path = list(sys.path)
+
+site.addsitedir('/usr/local/virtualenvs/mikroact/lib/python2.6/site-packages/')
+
+# reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+	if item not in prev_sys_path:
+		new_sys_path.append(item)
+		sys.path.remove(item)
+
+sys.path[:0] = new_sys_path
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+# FIXME hard-coded variables
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mikroact.settings.supervacuo")
+os.environ.setdefault("SECRET_KEY", "not too secret")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+import os
