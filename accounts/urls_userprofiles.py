e344eceaf20304b142853cf191e9da7278a4eef1
Carl van Tonder <carl@supervacuo.com>
2013-04-22 15:28:23 -0400

    Make password reset work
    Closes #21

--- accounts/urls_userprofiles.py
+++ accounts/urls_userprofiles.py
@@ -6,7 +6,14 @@ from .views import UserRegisterView, UserRegisterCompleteView, UserUpdateView, U
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'login/$', 'login', {'template_name': 'accounts/login.html'}, name='user_login'), 
     url(r'logout/$', 'logout', {'template_name': 'accounts/logout.html'}, name='user_logout'), 
-    url(r'^password_reset/$', 'password_reset', name='user_password_reset')
+    url(r'^password_reset/$', 'password_reset', name='user_password_reset'),
+    url(r'^password_reset/done/$', 'password_reset_done', name='user_password_reset_done'),
+    url(r'^password_reset/confirm/$', 'password_reset_done', name='user_password_reset_done'),
+    url(r'^password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        'password_reset_confirm', name='user_password_reset_confirm'),
+    url(r'^password_reset/complete/$', 'password_reset_complete',
+        name='user_password_reset_complete')
+
 )
 
 urlpatterns += patterns('',
