27b97cafe8836f930d34f6301b916cf53da41635
Carl van Tonder <carl@supervacuo.com>
2013-04-26 16:14:20 -0400

    Add django-debug-toolbar for devel sites

--- mikroact/settings/local.py
+++ mikroact/settings/local.py
@@ -20,3 +20,6 @@ DATABASES = {
 INTERNAL_IPS = ('127.0.0.1',)
 
 EMAIL_PORT = 1025
+
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
