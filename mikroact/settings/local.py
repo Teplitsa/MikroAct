9e5ce6590f85efae47cdb2fd87a17512b4bbd4d5
Carl van Tonder <carl@supervacuo.com>
2013-04-29 16:24:05 -0400

    Turn off INTERCEPT_REDIRECTS for local dev

--- mikroact/settings/local.py
+++ mikroact/settings/local.py
@@ -23,3 +23,7 @@ EMAIL_PORT = 1025
 
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INSTALLED_APPS += ('debug_toolbar',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False
+}
