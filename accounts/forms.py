Teplitsa/MikroAct

commit 53f7afd9e89807e8038cc6f874ba80a92a80fcf1

Tidy up user edit form:
* remove username, password & password2 fields
* reorder so email address field comes first

accounts/forms.py
@@ -6,14 +6,9 @@ from .models import UserProfile, Collective
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(), 
-                                label="Password", required=False)
-    password2 = forms.CharField(widget=forms.PasswordInput(), 
-                                label="Confirm password", required=False)
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('email',)
 
     def clean_password2(self):
         password = self.cleaned_data.get("password")

accounts/templates/accounts/user_form.html
@@ -20,9 +20,9 @@
 		<div class="span8 offset2">
 			<form  class="createForm" enctype="multipart/form-data" action="." method="post">
 				{% csrf_token %}
-				{{ form|as_bootstrap }}
 				{{ user_form|as_bootstrap }}
 				{{ user_form.errors }}
+				{{ form|as_bootstrap }}
 				{{ form.errors }}
 				<button type="submit">Save</button>
 			</form>

commit 78ae284828ace05f5c55ee2012d815710b6b0020

Fix crashes on user registration caused by 53f7afd
Also assign user permissions on own user profile when registering

accounts/forms.py
@@ -10,6 +10,16 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('email',)
 
+
+class RegistrationForm(UserForm):
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput(), 
+                                label="Confirm password")
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+
     def clean_password2(self):
         password = self.cleaned_data.get("password")
         password2 = self.cleaned_data.get("password2")
@@ -25,14 +35,6 @@ class UserForm(forms.ModelForm):
         if commit:
             user.save()
         return user
-
-
-class RegistrationForm(UserForm):
-    def __init__(self, *args, **kwargs):
-        self.base_fields['password'].required = True
-        self.base_fields['password2'].required = True
-
-        return super(RegistrationForm, self).__init__(*args, **kwargs)
 
 
 class UserProfileForm(forms.ModelForm):

accounts/views.py
@@ -108,6 +108,9 @@ class UserRegisterView(CreateView):
         user_profile = UserProfile(user=user)
         user_profile.save()
 
+        assign("change_userprofile", user, user_profile)
+        assign("delete_userprofile", user, user_profile)
+
         user.backend='django.contrib.auth.backends.ModelBackend'
         login(self.request, user)
 